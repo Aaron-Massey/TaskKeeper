0944ad627f293543ca815dcb91aa6f03ea0f9109
Aaron <03masseyAaron@gmail.com>
2026-07-01 05:35:46 -0500

    implemented basic notStarted
    This only has basic errors and logs. We need to decide what states have
    edges between them

---
+++ client/scripts/states/notStartedState.ts
@@ -0,0 +1,24 @@
+import { taskState } from './taskState'
+import { taskItem } from '../components/taskItem'
+import { inProgressState } from './inProgressState'
+
+export class notStartedState implements taskState {
+
+  public startTask(task: taskItem): void {
+    console.log('Starting task: ${task.title}')
+    task.setState(new inProgressState());
+  }
+
+  public blockTask(task: taskItem): void {
+    console.log('Blocking task: $task.title')
+  }
+
+  public completeTask(task: taskItem): void {
+    console.error("Cannot complete a task that hasn't been started.")
+  }
+
+  public archiveTask(task: taskItem): void {
+    console.log('Archiving task: ${task.title}')
+    task.setState(new archiveTask());
+  }
+}
