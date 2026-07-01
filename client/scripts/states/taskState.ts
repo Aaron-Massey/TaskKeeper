b716c63dda0ee06054363aa570fd8d3df4b1ae52
Aaron <03masseyAaron@gmail.com>
2026-07-01 05:27:20 -0500

    added basic structure

---
+++ client/scripts/states/taskState.ts
@@ -0,0 +1,8 @@
+import { taskItem } from '../components/taskItem';
+
+export interface taskState {
+  startTask(task: taskItem): void;
+  blockTask(task: taskItem): void;
+  completeTask(task: taskItem): void;
+  archiveTask(task: taskItem): void;
+}
