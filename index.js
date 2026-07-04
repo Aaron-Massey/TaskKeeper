fe69774df33a4163e4d9dd6e237e4e3c0366bbcb
cindyduong-ou <cindy.duong-1@ou.edu>
2026-07-03 21:30:59 -0500

    feat: import Task and TaskList composite classes into server entry point

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import http from 'http';
 import loadStatic from "./server/load_static.js";
+import { Task, TaskList } from './server/composite.js';
 
-const http_server = http.createServer(loadStatic)
+const http_server = http.createServer(loadStatic);
 
 const defaultPort = 3000;
 http_server.listen(process.env.PORT || defaultPort, () => {
